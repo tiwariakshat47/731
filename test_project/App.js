536bc2f871c83fca13c729d660df18e04d12f267
Tony Wu <wuzhentony@gmail.com>
2024-03-07 01:13:27 -0800

    upload test_project 1

--- test_project/App.js
+++ test_project/App.js
@@ -1,1 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from "react-redux"
+import { store } from './store';
 
+export default function App() {
+  return (
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>edison is dumb!</Text>
+      </View>
+    </Provider> 
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
